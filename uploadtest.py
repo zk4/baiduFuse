87d0ceb46927f1f211faa7533c4be26ff645baaa
zk <liuzq7@gmail.com>
2019-10-07 20:52:13 +0800

    demo

--- uploadtest.py
+++ uploadtest.py
@@ -1,55 +1,10 @@
 import requests
 import json
-import io
-class CancelledError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-        Exception.__init__(self, msg)
+from core.progress_requests import BufferReader, progress
+from core.autoBDUSS import getBDUSS
 
-    def __str__(self):
-        return self.msg
+BDUSS = getBDUSS()
 
-    __repr__ = __str__
-
-class BufferReader(io.BytesIO):
-    def __init__(self, buf=b'',
-                 callback=None,
-                 cb_args=(),
-                 cb_kwargs={}):
-        self._callback = callback
-        self._cb_args = cb_args
-        self._cb_kwargs = cb_kwargs
-        self._progress = 0
-        self._len = len(buf)
-        io.BytesIO.__init__(self, buf)
-
-    def __len__(self):
-        return self._len
-
-    def read(self, n=-1):
-        chunk = io.BytesIO.read(self, n)
-        self._progress += int(len(chunk))
-        self._cb_kwargs.update({
-            'size'    : self._len,
-            'progress': self._progress
-        })
-        if self._callback:
-            try:
-                self._callback(*self._cb_args, **self._cb_kwargs)
-            except: # catches exception from the callback
-                raise CancelledError('The upload was cancelled.')
-        return chunk
-
-
-def progress(size=None, progress=None):
-    print("{0} / {1}".format(size, progress))
-BDUSS = ""
-import browser_cookie3
-cj = browser_cookie3.chrome(domain_name='baidu.com')
-for cookie in cj:
-    if cookie.name =='BDUSS':
-        BDUSS=cookie.value
-print(BDUSS)
 user_agent="netdisk;8.3.1;android-android"
 app_id= 266719
 
@@ -58,7 +13,7 @@ def upload(path):
     querystring = {"app_id":"266719","method":"upload","type":"tmpfile"}
     files = {"myFileUpl": ("name", open(path, 'rb').read())}
 
-    (data, ctype) = requests.packages.urllib3.filepost.encode_multipart_formdata(files)      
+    (data, ctype) = requests.packages.urllib3.filepost.encode_multipart_formdata(files)
 
     body = BufferReader(data, progress)
     headers = {
@@ -70,7 +25,6 @@ def upload(path):
  
     response = requests.request("POST", url,data=body , headers=headers, params=querystring)
     return json.loads(response.text)["md5"]
-
 
 def createSuperFile(blocklist,path):
     url = "http://pcs.baidu.com/rest/2.0/pcs/file"
