443cf92c4119d0c1de201a635f2b7478f8291072
zk <liuzq7@gmail.com>
2019-10-07 20:30:33 +0800

    request monitor demo

--- uploadtest.py
+++ uploadtest.py
@@ -1,23 +1,75 @@
 import requests
 import json
-# with open('report.xls', 'rb') as f:
-#     r = requests.post('http://httpbin.org/post', files={'report.xls': f})
+import io
+class CancelledError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        Exception.__init__(self, msg)
 
-BDUSS=""
+    def __str__(self):
+        return self.msg
+
+    __repr__ = __str__
+
+class BufferReader(io.BytesIO):
+    def __init__(self, buf=b'',
+                 callback=None,
+                 cb_args=(),
+                 cb_kwargs={}):
+        self._callback = callback
+        self._cb_args = cb_args
+        self._cb_kwargs = cb_kwargs
+        self._progress = 0
+        self._len = len(buf)
+        io.BytesIO.__init__(self, buf)
+
+    def __len__(self):
+        return self._len
+
+    def read(self, n=-1):
+        chunk = io.BytesIO.read(self, n)
+        self._progress += int(len(chunk))
+        self._cb_kwargs.update({
+            'size'    : self._len,
+            'progress': self._progress
+        })
+        if self._callback:
+            try:
+                self._callback(*self._cb_args, **self._cb_kwargs)
+            except: # catches exception from the callback
+                raise CancelledError('The upload was cancelled.')
+        return chunk
+
+
+def progress(size=None, progress=None):
+    print("{0} / {1}".format(size, progress))
+BDUSS = ""
+import browser_cookie3
+cj = browser_cookie3.chrome(domain_name='baidu.com')
+for cookie in cj:
+    if cookie.name =='BDUSS':
+        BDUSS=cookie.value
+print(BDUSS)
 user_agent="netdisk;8.3.1;android-android"
 app_id= 266719
 
 def upload(path):
     url = "http://pcs.baidu.com/rest/2.0/pcs/file"
     querystring = {"app_id":"266719","method":"upload","type":"tmpfile"}
+    files = {"myFileUpl": ("name", open(path, 'rb').read())}
+
+    (data, ctype) = requests.packages.urllib3.filepost.encode_multipart_formdata(files)      
+
+    body = BufferReader(data, progress)
     headers = {
         'host': "pcs.baidu.com",
         'User-Agent':user_agent,
-        'cookie': "BDUSS="+BDUSS
+        'cookie': "BDUSS="+BDUSS,
+        'Content-Type':ctype
         }
-    with open(path,"rb") as f:
-        response = requests.request("POST", url,files={path:f} , headers=headers, params=querystring)
-        return json.loads(response.text)["md5"]
+ 
+    response = requests.request("POST", url,data=body , headers=headers, params=querystring)
+    return json.loads(response.text)["md5"]
 
 
 def createSuperFile(blocklist,path):
@@ -35,5 +87,7 @@ def createSuperFile(blocklist,path):
     }
     response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 
-md5 = upload("./demotest.py")
-createSuperFile([md5],"/test/hhhhhhhhhhhhhhhhhhhhhhhhhh.py")
+path ="/Users/zk/Downloads/o.mp4"
+md5 = upload(path)
+createSuperFile([md5],"/test/ooo.py")
+
