9da4b539d1a34d254b474ca95d0367a0b7471bbf
zk <liuzq7@gmail.com>
2019-10-08 14:13:46 +0800

    tidy

--- core/task.py
+++ core/task.py
@@ -14,16 +14,12 @@ from log import logger,funcLog
 from core.scheduler import handle,q,session
 from core.custom_exceptions import Baidu8Secs
 from utils.utils import notification
-logger.setLevel(logging.DEBUG)
-
-
 
 class Task(object):
     @staticmethod
     def createMmap(filename,size, access=mmap.ACCESS_WRITE):
         fd = os.open(filename, os.O_RDWR)
         return mmap.mmap(fd, size, access=access)
-
 
     @staticmethod
     def createHelperThread(startIdx,endIdx,task):
@@ -42,9 +38,7 @@ class Task(object):
     def __init__(self,url,saved_path,headers=None):
 
          
-#         logger.debug(saved_path)
         # almost all the files need to read fast , other wise the app will frozen or exit 
-
 #         previewableExts={ "mkv","mpv","mp3","mp4","flv","ts","mov","avi","aac","flac","asf","rma","rmvb", \
 #                 "rm","ogg","mpeg","vob","m4a","wma","wmv","3gp","zip","rar","tar","7z","pdf","doc","docx","xls","xlsx","dmg" }
 
@@ -60,21 +54,17 @@ class Task(object):
         self.user_headers=headers
 
         self.part_size = 65536*4
-
-        self.cache =[]
+        self.block_infos =[]
         self.current_file_size = 0
         self.file_size =0
         self.terminating = False
-        self.m = None
+        self.mmap = None
         self.part_count=None
-#         self.condition = threading.Condition()
-        
-
 
     def get_cache(self,offset,size):
         try:
             r = self.get_block_range(offset,size)
-            c = self.cache[r[0]]
+            c = self.block_infos[r[0]]
             start = time.time()
 
             maxWaitRound = 10
@@ -82,38 +72,30 @@ class Task(object):
             while True:
                 if  r[0] == r[1]:
                     if c["cur"]+c["start"]>=(size+offset):
-                        # tiny data
-#                         logger.info(f"tiny data found,{self.saved_path} - {c} ")
-                        return self.m[offset:offset+size]
+                        return self.mmap[offset:offset+size]
                     else:
                         with c["m"]:
                             c["m"].wait(1)
                 else:
                     alldone = True
                     for i in range(r[0],r[1]+1):
-                        if self.cache[i]["status"] != "done":
+                        if self.block_infos[i]["status"] != "done":
                             alldone=False
-                            with self.cache[i]["m"]:
-                                self.cache[i]["m"].wait(1)
-
+                            with self.block_infos[i]["m"]:
+                                self.block_infos[i]["m"].wait(1)
                     if alldone:
-#                         logger.info(f'range - {r}')
-                        return self.m[offset:offset+size]
+                        return self.mmap[offset:offset+size]
 
-
-                
                 q.put((Task.createHelperThread,[r[0],r[1],self],1))
                 end = time.time()
                 # no response for 10 secs, just drop it 
+                # TODO this  shoud be configurable
                 if end-start>10:
                     return None
                 curRound+=1
-#                 logger.debug(f"wait for {curRound}")
-#                 with self.condition:
-#                     self.condition.wait(1)
 
         except Exception as e:
-            logger.debug(f'{r[0]},{len(self.cache)},{self.saved_path}')
+            logger.debug(f'{r[0]},{len(self.block_infos)},{self.saved_path}')
             logger.exception(e) 
 
 
@@ -131,7 +113,6 @@ class Task(object):
         try: 
             self.file_size = int(r.headers["content-length"]) 
             if 'Location' in r.headers and 'issuecdn' in r.headers['Location']:
-
                 notification(self.saved_path[self.saved_path.rfind("/")+1:],"文件已变 8 秒~")
                 raise Baidu8Secs(self.saved_path)
         except Exception as e : 
@@ -143,11 +124,12 @@ class Task(object):
             fp.seek(self.file_size)
             fp.write(b'\0')
             
-        self.m = Task.createMmap(self.saved_path,self.file_size)
+        self.mmap = Task.createMmap(self.saved_path,self.file_size)
         self.create_range()
         # pre start task to get data 
         if self.isPreviewAble:
             q.put((Task.createHelperThread,[0,8 if 8 < self.part_count else self.part_count-1 ,self],1))
+            #  request the end for a little bit 
 #             q.put((createHelperThread,[self.part_count-3 if ( self.part_count-3 ) > 0   else self.part_count-1 ,self.part_count-1,self],1))
         else:
             q.put((Task.createHelperThread,[0,400 if 400 < self.part_count else self.part_count-1 ,self],1))
@@ -160,7 +142,7 @@ class Task(object):
         while start < self.file_size:
             if start+size> self.file_size:
                 size = self.file_size - start
-            self.cache.append({"status":None,"start":start,"size":size,"cur":0,"m":threading.Condition() })
+            self.block_infos.append({"status":None,"start":start,"size":size,"cur":0,"m":threading.Condition() })
             if size == self.file_size - start:
                 break
             start += self.part_size
