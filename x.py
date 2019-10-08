05fe5e54bc429b3eb080effbdd42a85991b5d42c
zk <liuzq7@gmail.com>
2019-10-08 22:31:18 +0800

    defend coding

--- x.py
+++ x.py
@@ -294,7 +294,8 @@ class CloudFS(Operations):
                 del self.writing_files[path]
 
                 # why ? prevent accidently read file when uploading still in progress
-                del self.downloading_files[path]
+                if path in self.downloading_files:
+                    del self.downloading_files[path]
                 
                 print("released",path)
                 return  
