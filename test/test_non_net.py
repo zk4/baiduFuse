5216d9e57535ec24397ace337dcdd4632091eab7
zk <liuzq7@gmail.com>
2019-10-14 16:28:27 +0800

    add test

--- test/test_non_net.py
+++ test/test_non_net.py
@@ -20,3 +20,15 @@
 # for i in range(*get_block_range(10,20)):
 #     print(i)
 
+import os
+def test_parent_and_filename():
+    names=["/","/a","/a/b","/a/bc.txt"]
+    expect_dir=["/","/","/a","/a"]
+    expect_name=["","a","b","bc.txt"]
+
+    for i,n in enumerate(names):
+
+        assert expect_name[i]==os.path.basename(names[i])
+        assert expect_dir[i]==os.path.dirname(names[i])
+        assert expect_name[i]==n[n.rfind("/")+1:]
+#      path[path.rfind("/")+1:]
